ca406d77bf0e5cf47745166013c89c35b3eeb3f1
Tyler Tessmann <tyelrx@gmail.com>
2015-01-30 16:55:33 -0600

    added func to struct

--- LionsAndTigers/Tiger.swift
+++ LionsAndTigers/Tiger.swift
@@ -14,4 +14,48 @@ struct Tiger {
     var age = 0
     var breed = ""
     var image = UIImage(named: "")
+    
+    func chuff (){
+        [println("Tiger : Chuff Chuff")]
+        // in a struct you can not acces vars of it's self/ only function
+        // thus self.age = 3 is invalid for stuct whereas a class you could
+    }
+    
+    func chuffANumberOfTimes (numberOfTimes:Int){
+        for var chuff = 0; chuff < numberOfTimes; ++chuff {
+            self.chuff()
+        }
+    
+    }
+    
+    func chuffANumberOfTimes (numberOfTimes:Int, isLoud:Bool){
+        for var chuffTimes = 1; chuffTimes <= numberOfTimes; chuffTimes++ {
+            if isLoud{
+                chuff()
+            }else{
+                println("Tiger: purr purr")
+            }
+        }
+    }
+    
+    func ageInTigerYearsFromAge(regularAge:Int) -> Int{
+        //let newAge = regularAge*3
+        //return newAge
+        //or
+        return regularAge * 3
+    }
+    
+    func randomFact() -> String{
+        let randomNumber = Int(arc4random_uniform(UInt32(3)))
+        var randomFact:String
+        
+        if randomNumber == 0 {
+            randomFact = "The Tiger is the biggest species in the cat family"
+        }else if randomNumber == 1{
+            randomFact = "Tigers can reach a length of 3.3 meters"
+        } else{
+            randomFact = "A group of tigers is known as an 'ambush' or 'streak"
+        }
+    return randomFact
+    }
 }
--- LionsAndTigers/ViewController.swift
+++ LionsAndTigers/ViewController.swift
@@ -22,6 +22,7 @@ class ViewController: UIViewController {
     let myTigerImageName = ["BengalTiger.jpg", "IndochineseTiger.jpg", "MalayanTiger.jpg", "SiberianTiger.jpg"]
     
     var myTigers:[Tiger] = []
+    var currentIndex = 0
     
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -33,12 +34,15 @@ class ViewController: UIViewController {
             myTiger = Tiger()
             myTiger.name = myTigerNames[x]//names array
             myTiger.breed = myTigerBreed[x]
-            myTiger.age = myTigerAge[x]
+            myTiger.age = myTiger.ageInTigerYearsFromAge(myTigerAge[x])
             myTiger.image = UIImage(named: myTigerImageName[x])
             myTigers.append(myTiger)
         }
         
         println(myTigers)
+        //myTigers[0].chuff();
+        myTigers[0].chuffANumberOfTimes(3)
+        myTigers[1].chuffANumberOfTimes(5, isLoud: false)
       
         myImageView.image = myTigers[0].image
         nameLabel.text = myTigers[0].name
@@ -53,12 +57,27 @@ class ViewController: UIViewController {
 
     @IBAction func nextBarButtonItemPressed(sender: UIBarButtonItem) {
         
-        let randomIndex = Int(arc4random_uniform(UInt32(myTigers.count)))
+        var randomIndex:Int
+        
+        do {
+         randomIndex = Int(arc4random_uniform(UInt32(myTigers.count)))
+        } while currentIndex == randomIndex
+        
+        currentIndex = randomIndex
+        
         let randomTiger = myTigers[randomIndex]
-        myImageView.image = randomTiger.image
-        nameLabel.text = randomTiger.name
-        ageLabel.text = "\(randomTiger.age)"
-        breedLabel.text = randomTiger.breed
+        
+        //UIViewAnimationOptions.TransitionCrossDissolve
+        //UIViewAnimationOptions.TransitionCurlUp
+        
+        UIView.transitionWithView(self.view, duration: 2, options: UIViewAnimationOptions.TransitionCrossDissolve, animations:{
+            self.myImageView.image = randomTiger.image
+            self.nameLabel.text = randomTiger.name
+            self.ageLabel.text = "\(randomTiger.age)"
+            self.breedLabel.text = randomTiger.breed
+            }, completion: {
+                (finished: Bool) -> () in
+        })
         
     }
 
